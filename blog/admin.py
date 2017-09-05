65a7721dbdc03ad966fa5502e253fd91bb75f6ea
Isaiah <isaiahnyabuto@gmail.com>
2017-09-05 04:02:56 +0300

    update

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -14,3 +14,11 @@ class PostAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Post, PostAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+# Register the model
+admin.site.register(Comment, CommentAdmin)
